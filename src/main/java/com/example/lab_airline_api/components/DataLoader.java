ae405590ac1b83a49d2eee3bc08dc7527784ca1f
Stella Annor <stella.annor@icloud.com>
2023-04-21 09:32:25 +0100

    updates dataloader

--- src/main/java/com/example/lab_airline_api/components/DataLoader.java
+++ src/main/java/com/example/lab_airline_api/components/DataLoader.java
@@ -1,4 +1,71 @@
 package com.example.lab_airline_api.components;
 
-public class DataLoader {
+import com.example.lab_airline_api.models.Flight;
+import com.example.lab_airline_api.models.Passenger;
+import com.example.lab_airline_api.repositories.FlightRepository;
+import com.example.lab_airline_api.repositories.PassengerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements ApplicationRunner {
+
+    @Autowired
+    FlightRepository flightRepository;
+
+    @Autowired
+    PassengerRepository passengerRepository;
+
+    public DataLoader(){
+
+    }
+    @Override
+    public void run(ApplicationArguments args) throws Exception{
+
+        Passenger passenger1 = new Passenger("Stella", "stella@bnta.com");
+        passengerRepository.save(passenger1);
+
+        Flight flightGhana = new Flight("Ghana", 200L, "29.04.2023", "18:30");
+        flightRepository.save(flightGhana);
+
+        Passenger passenger2 = new Passenger("Anna", "Anna@bnta.com");
+        passengerRepository.save(passenger2);
+
+        Flight flightSpain =  new Flight("Spain", 150L, "14.05.2023", "08:30");
+        flightRepository.save(flightSpain);
+        Flight flightFrance = new Flight("France", 201L, "11.07.2023", "05:30");
+        flightRepository.save(flightFrance);
+
+        Passenger passenger3 = new Passenger("Florian", "florian@gmail.com");
+        passengerRepository.save(passenger3);
+
+        flightFrance.addPassenger(passenger1);
+        flightFrance.addPassenger(passenger2);
+        flightSpain.addPassenger(passenger2);
+        flightGhana.addPassenger(passenger1);
+        flightGhana.addPassenger(passenger3);
+
+        flightRepository.save(flightFrance);
+        flightRepository.save(flightSpain);
+        flightRepository.save(flightGhana);
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
 }
